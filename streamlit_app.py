e3d2b4e86a5b301e3deb974118f975a3bc1848ec
tim-home <timmycheng@foxmail.com>
2024-11-24 22:32:26 +0800

    init

---
+++ streamlit_app.py
@@ -0,0 +1,21 @@
+# a demo of streamlit
+
+import streamlit as st
+
+# 设置页面标题
+st.title("欢迎来到我的 Streamlit 应用")
+
+# 添加一些文本
+st.write("这是一个简单的 Streamlit 应用示例。")
+
+# 添加一个按钮
+if st.button('点击我'):
+    st.write('你点击了按钮！')
+
+# 添加一个滑块
+age = st.slider('选择你的年龄', 0, 100, 25)
+st.write(f'你的年龄是 {age} 岁')
+
+# 添加一个输入框
+name = st.text_input('请输入你的名字')
+st.write(f'你好，{name}！')
